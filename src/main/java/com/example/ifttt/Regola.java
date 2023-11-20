ec3015ced4e55afc622bcc7419f0f95473c96e6f
Antony Storti <antony.storti@protonmail.com>
2023-11-20 16:20:07 +0100

    Initial Configuration: JavaFX

--- src/main/java/com/example/ifttt/Regola.java
+++ src/main/java/com/example/ifttt/Regola.java
@@ -1,5 +1,6 @@
 package com.example.ifttt;
 
+import java.time.DayOfWeek;
 import java.time.LocalTime;
 
 public class Regola {
@@ -7,17 +8,26 @@ public class Regola {
     Trigger t;
     Azione a;
     boolean stato;
+    boolean ripetibile;
     LocalTime quiescenza;
 
 
     //---COSTRUTTORE---//
-    public Regola(Trigger t, Azione a, boolean stato, LocalTime quiescenza) {
+    public Regola(Trigger t, Azione a, boolean stato, boolean ripetibile, LocalTime quiescenza) {
         this.t = t;
         this.a = a;
         this.stato = stato;
+        this.ripetibile = ripetibile;
         this.quiescenza = quiescenza;
     }
     //-----------------//
+
+
+    private boolean valutaAttivazione(){
+
+        return false;
+
+    }
 
 
 }
