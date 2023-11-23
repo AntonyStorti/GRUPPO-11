9b59751ae9c7320006a19dee6276f8bd9e7d84d9
Antony Storti <antony.storti@protonmail.com>
2023-11-23 13:25:21 +0100

    Initial Configuration: JavaFX

--- src/main/java/com/example/ifttt/HelloController.java
+++ src/main/java/com/example/ifttt/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.ifttt;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
@@ -32,5 +33,11 @@ public class HelloController {
         } catch (Exception e) {
             e.printStackTrace(); // Gestisci l'eccezione in modo adeguato
         }
+    }
+
+    public void eliminaRegola(ActionEvent actionEvent) {
+    }
+
+    public void modificaRegola(ActionEvent actionEvent) {
     }
 }
